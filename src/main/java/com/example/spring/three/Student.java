82d0892c0f1dbf578bd41d59fa6ce35c84a8c778
Mohamed Sakhri <mohamed.sakhri@gmail.com>
2016-08-05 15:41:44 +0200

    SPRING-04: Explicitly define constructor's parameters in the bean's config

---
+++ src/main/java/com/example/spring/three/Student.java
@@ -0,0 +1,82 @@
+package com.example.spring.three;
+
+/**
+ * 
+ * @author Mohamed Sakhri
+ *
+ * This class represents a student. As for the start it has only basic properties and functionalities.
+ */
+
+public class Student {
+	
+	private String firstName;
+	private String lastName;
+	private int age;
+	
+	/**
+	 * 
+	 * @param name Student's first name
+	 * @param age Student's age in years
+	 */
+	public Student(String firstName, String lastName, int age) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+	
+	/**
+	 * 
+	 * @return Student's first name
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	/**
+	 * 
+	 *   param name Student's first first name
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	/**
+	 * 
+	 * @return Student's last name
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	
+	/**
+	 * 
+	 *   param name Student's last name
+	 */
+	public void setName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	/**
+	 * 
+	 * @return Student's age in years
+	 */
+	public int getAge() {
+		return age;
+	}
+	
+	/**
+	 * 
+	 * @param age Student's age in years
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	/**
+	 * 
+	 * @param message to be displayed
+	 */
+	public void greeting(String message) {
+		System.out.println("Hello! I'm " + getFirstName() + " " + getLastName() + ". I'm " + getAge() + ". " + message);
+	}
+}
