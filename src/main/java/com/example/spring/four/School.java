284e614f0f1b8d2196314e340c7b017a16c09a65
Mohamed Sakhri <mohamed.sakhri@gmail.com>
2016-08-05 20:15:35 +0200

    SPRING-05: Using spring setter-based dependency injection

---
+++ src/main/java/com/example/spring/four/School.java
@@ -0,0 +1,43 @@
+package com.example.spring.four;
+
+/**
+ * 
+ * @author Mohamed Sakhri
+ *
+ * This class represents a school. As for the start it has only basic properties and functionalities.
+ */
+
+public class School {
+	
+	private String name;
+
+	/**
+	 * 
+	 * @param name School's name
+	 */
+	public School(String name) {
+		super();
+		this.name = name;
+	}
+
+	/**
+	 * 
+	 * @return School's name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * 
+	 * @param name School's name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void greeting() {
+		System.out.println("Welcome to " + getName());
+	}
+	
+}
