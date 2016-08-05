d3bf17a17e1784c3b5022fa17d16366ffb718093
Mohamed Sakhri <mohamed.sakhri@gmail.com>
2016-08-05 15:13:35 +0200

    SPRING-03: using spring constructor-based dependency injection

---
+++ src/main/java/com/example/spring/two/Application.java
@@ -0,0 +1,28 @@
+package com.example.spring.two;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 
+ * @author Mohamed Sakhri
+ *
+ * Application to demonstrate how spring framework can be used.
+ * 
+ */
+
+public class Application {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		// Defining where to get the beans' configuration from 
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/example/spring/two/springConfig.xml");
+
+		// Get the bean with the id student. It's the only bean we have defined 
+		Student student = (Student) context.getBean("student");
+		
+		// The name and the age should be taken care of by the bean configuration as constuctor's parameters
+		student.greeting("Greeting from Spring using spring constructor-based dependency injection");
+	}
+
+}
---
+++ src/main/java/com/example/spring/two/Student.java
@@ -0,0 +1,64 @@
+package com.example.spring.two;
+
+/**
+ * 
+ * @author Mohamed Sakhri
+ *
+ * This class represents a student. As for the start it has only basic properties and functionalities.
+ */
+
+public class Student {
+	
+	private String name;
+	private int age;
+	
+	/**
+	 * 
+	 * @param name Student's name
+	 * @param age Student's age in years
+	 */
+	public Student(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	/**
+	 * 
+	 * @return Student's name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * 
+	 *   param name Student's name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * 
+	 * @return Student's age in years
+	 */
+	public int getAge() {
+		return age;
+	}
+	
+	/**
+	 * 
+	 * @param age Student's age in years
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	/**
+	 * 
+	 * @param message to be displayed
+	 */
+	public void greeting(String message) {
+		System.out.println("Hello! I'm " + getName() + ". I'm " + getAge() + ". " + message);
+	}
+}
